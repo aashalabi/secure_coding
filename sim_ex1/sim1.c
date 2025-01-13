11dc983860a1c49b021646ef6d5193b763bc518c
aashalabi <ahmed.shalabi@gmail.com>
2025-01-12 23:26:47 -0800

    add fix security

--- sim_ex1/sim1.c
+++ sim_ex1/sim1.c
@@ -14,7 +14,7 @@ int main(int argc, char** argv) {
         fprintf(stderr, "Please provide the address of a file as an input.\n");
         return -1;
     }
-    char cmd[BUFSIZE] = "wc -c < ";
+    
     char *file_name = argv[1];
 
     // Use stat instead of system command to get file size
@@ -29,28 +29,4 @@ int main(int argc, char** argv) {
 
     return EXIT_SUCCESS;
 
-/*
-    //check if the file exits
-    FILE *file = fopen(file_name, "rb");
-    if (!file) {
-        perror("Files doesn't exist");
-        return EXIT_FAILURE;
-    }
-    else {
-        fclose(file);
-    }
-
-    //check for over follow
-    int allowed_len  = BUFSIZE - strlen(cmd) -1;
-    if (allowed_len >= strlen(file_name)) {
-        //copy argument
-        strncat(cmd, file_name, allowed_len);
-    }
-    else{
-        perror("Invalid file size");
-        return EXIT_FAILURE;
-    }
-
-    system(cmd);
-*/
 }
